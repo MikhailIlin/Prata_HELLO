fe416eec8831fde83c1618c798a321f8219ab978
MikhalIlin <pochta_vechna@bk.ru>
2018-07-15 20:17:56 +0300

    initial commit

---
+++ Debug/Prata.d
@@ -0,0 +1,1 @@
+Prata.o: ../Prata.cpp
---
+++ Prata.cpp
@@ -0,0 +1,19 @@
+/*
+ * Prata.cpp
+ *
+ *  Created on: 13 июл. 2018 г.
+ *      Author: user
+ */
+// Prata.cpp - выводит информацию на экран
+#include <iostream>
+int main()
+{
+	using namespace std;
+	cout << "Come up and C++ me some time.";
+	cout << endl;
+	cout << "You won't regred it!" << endl;
+	return 0;
+}
+
+
+
